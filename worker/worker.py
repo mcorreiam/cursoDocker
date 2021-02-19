6c67c279a41e7e5fb3cc09472d48b933b1b492ba
Marco Correia <m.correia.m@gmail.com>
2021-02-19 15:55:10 -0300

    Aula 60 Multiplas instancias

--- worker/worker.py
+++ worker/worker.py
@@ -4,6 +4,7 @@ from time import sleep
 from random import randint
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+    print('Aguardando mensagens...')
     while True:
         mensagem = json.loads(r.blpop('sender')[1])
         print('Mandando a mensagem:', mensagem['assunto'])
